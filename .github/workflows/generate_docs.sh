7fe217b22f62190a6515d5bf27567d364a0c6d9c
mkhytarmkhoian <mkhytar.mkhoian@gmail.com>
2024-02-20 12:36:29 +0200

    Setup CI

---
+++ .github/workflows/generate_docs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# The website is built using MkDocs with the Material theme.
+# https://squidfunk.github.io/mkdocs-material/
+# It requires Python to run.
+# Install the packages with the following command:
+# pip install mkdocs mkdocs-material mkdocs-redirects
+
+set -ex
+
+# Generate the API docs
+./gradlew dokkaHtml
+mv ./build/dokka/api docs/docs
+
+# Build the site locally
+cd ./docs
+python3 -m venv venv
+source venv/bin/activate
+pip3 install mkdocs-material
+mkdocs build
